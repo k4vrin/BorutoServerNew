1e11eafbf31116ca045c67763e655bba86e41c5c
Mostafa Hosseini <kavrindev@gmail.com>
2022-06-03 21:46:22 +0430

    add root endpoint

--- src/main/kotlin/com/kavrin/plugins/Routing.kt
+++ src/main/kotlin/com/kavrin/plugins/Routing.kt
@@ -1,14 +1,13 @@
 package com.kavrin.plugins
 
+import com.kavrin.routes.root
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        // Routes have been defined in routes package
+        root()
     }
 }
